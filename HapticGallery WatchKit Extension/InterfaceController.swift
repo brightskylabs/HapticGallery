ab67405dab2fb1ea7ce724dc93dd8cd703b737ab
Sean White <swhite@starry.net>
2015-09-14 15:48:57 -0700

    Added logic for haptic feedback

--- HapticGallery WatchKit Extension/InterfaceController.swift
+++ HapticGallery WatchKit Extension/InterfaceController.swift
@@ -11,11 +11,40 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    
+    @IBOutlet var myPicker: WKInterfacePicker!
+    @IBOutlet var itemLabel: WKInterfaceButton!
+    var currentHaptic: Int = 1
+    
+    var itemList: [(String)] = [
+        ("Notification"),
+        ("DirectionUp"),
+        ("DirectionDown"),
+        ("Success"),
+        ("Failure"),
+        ("Retry"),
+        ("Start"),
+        ("Stop"),
+        ("Click")
+        ]
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        // Configure interface objects here.
+//        var hapticItems = [String]()
+//        var m = 0
+//        while let hapticItem = WKHapticType.init(rawValue: m) {
+//            hapticItems.append(hapticItem)
+//            m++
+//        }
+//        
+        let pickerItems: [WKPickerItem] = itemList.map {
+            let pickerItem = WKPickerItem()
+            pickerItem.title = $0
+            return pickerItem
+        }
+        
+        myPicker.setItems(pickerItems)
     }
 
     override func willActivate() {
@@ -26,6 +55,14 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    @IBAction func pickerChanged(value: Int) {
+        //itemLabel.setText(itemList[value].1)
+        currentHaptic = value
+    }
+    @IBAction func feelButtonPressed() {
+        WKInterfaceDevice.currentDevice().playHaptic(WKHapticType.init(rawValue: currentHaptic)!);
     }
 
 }
